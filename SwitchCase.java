1c1f10d2ad1be9f71894cbb1f6037b6064984fb8
Rodrigo Bossini <professorbossini@gmail.com>
2022-03-18 11:38:39 -0300

    Tratamento de nota invalida no SwitchCase

--- SwitchCase.java
+++ SwitchCase.java
@@ -4,5 +4,32 @@ public class SwitchCase {
     public static void main(String[] args) {
        int nota;
        nota = Integer.parseInt(JOptionPane.showInputDialog("Digite a nota"));
+       switch(nota){
+           case 10:
+                JOptionPane.showMessageDialog(null, "Parabéns");
+                JOptionPane.showMessageDialog(null, "A");
+                break;
+            case 9:
+                JOptionPane.showMessageDialog(null, "A");
+                break;
+            case 8:
+                JOptionPane.showMessageDialog (null, "B");
+                break;
+            case 7:
+                JOptionPane.showMessageDialog (null, "C");
+                break;
+            case 6:
+            case 5:
+            case 4:
+            case 3:
+            case 2:
+            case 1:
+            case 0:
+                JOptionPane.showMessageDialog(null, "R");
+                break;
+            default:
+                JOptionPane.showMessageDialog (null, "Nota inválida");
+                break;
+       }
     }
 }

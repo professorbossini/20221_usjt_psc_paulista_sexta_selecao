dfa60b3796c8be12fab649c36d0370fd44e9ee06
Rodrigo Bossini <professorbossini@gmail.com>
2022-03-18 10:25:11 -0300

    IfElse encerrado

--- IfElse.java
+++ IfElse.java
@@ -2,7 +2,23 @@ import java.util.Scanner;
 public class IfElse {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
+        
+        System.out.println("Digite sua nota");
+        byte nota = s.nextByte();
 
+        if (nota >= 70){
+            System.out.println ("Parabéns");
+            System.out.println ("Aprovado");        
+        }
+        else
+            System.out.println ("Reprovado");
+        
+        // if (nota >= 70){
+        //     System.out.println ("Aprovado");
+        // }
+        // if (nota < 70){
+        //     System.out.println ("Reprovado");
+        // }
 
         s.close();
     }

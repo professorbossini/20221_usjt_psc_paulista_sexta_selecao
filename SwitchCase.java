0a736646bd6489a7b3c1f9e77001d95cdf3e8c66
Rodrigo Bossini <professorbossini@gmail.com>
2022-03-18 11:31:29 -0300

    SwitchCase pronto

---
+++ SwitchCase.java
@@ -0,0 +1,8 @@
+import javax.swing.JOptionPane;
+
+public class SwitchCase {
+    public static void main(String[] args) {
+       int nota;
+       nota = Integer.parseInt(JOptionPane.showInputDialog("Digite a nota"));
+    }
+}
